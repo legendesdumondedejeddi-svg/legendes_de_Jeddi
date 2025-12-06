cfde6cf5a158176b886e575000f43e5839b4fd08
legendesdumondedejeddi-svg <legendes.du.monde.de.jeddi@gmail.com>
2025-12-06 06:30:34 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,23 +1,45 @@
-from flask import Flask, render_template
-from flask_babel import Babel, gettext
+from flask import Flask, render_template, request, session, url_for, redirect
+import json
 
 app = Flask(__name__)
-app.config['BABEL_DEFAULT_LOCALE'] = 'fr'
-app.config['BABEL_TRANSLATION_DIRECTORIES'] = 'translations'
+app.secret_key = "jeddi"
 
-babel = Babel(app)
+# Charger les traductions
+with open("translations.json", "r", encoding="utf8") as f:
+    translations = json.load(f)
 
-@babel.localeselector
-def get_locale():
-    return 'fr'  # tu peux changer après pour gérer l’auto-détection
+def get_lang():
+    return request.args.get("lang", "fr")
 
-@app.route('/')
+def T(lang):
+    return translations.get(lang, translations["fr"])
+
+@app.route("/")
 def home():
-    return render_template('home.html', title=gettext("Accueil"))
+    lang = get_lang()
+    return render_template("home.html", t=T(lang), lang=lang, title=T(lang)["title_home"])
 
-@app.route('/about')
+@app.route("/about")
 def about():
-    return render_template('about.html', title=gettext("À propos"))
+    lang = get_lang()
+    return render_template("about.html", t=T(lang), lang=lang, title=T(lang)["title_about"])
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route("/don")
+def don():
+    lang = get_lang()
+    return render_template("don.html", t=T(lang), lang=lang, title=T(lang)["title_don"])
+
+@app.route("/grimoire")
+def grimoire():
+    lang = get_lang()
+    return render_template("grimoire.html", t=T(lang), lang=lang, title=T(lang)["title_grimoire"])
+
+@app.route("/legendes")
+def liste_legendes():
+    lang = get_lang()
+    return render_template("liste_legendes.html", t=T(lang), lang=lang, title=T(lang)["title_legendes"])
+
+@app.route("/legende/<slug>")
+def legende(slug):
+    lang = get_lang()
+    return render_template("legende.html", slug=slug, t=T(lang), lang=lang, title=T(lang)["title_legendes"])
