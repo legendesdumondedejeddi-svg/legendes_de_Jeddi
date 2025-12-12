f8e4dd5a3f8cd87a08139db8346a46101807bffc
Jeddi Tratrick <legendes.du.monde.de.jeddi@gmail.com>
2025-12-12 18:38:24 +0100

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('.lang-switch .submenu a').forEach(link => {
-    link.addEventListener('click', e => {
-        e.preventDefault();
-        const lang = link.getAttribute('href').split('=')[1];
-        alert('Sélection de la langue : ' + lang + '\n(Le chargement des traductions peut être implémenté ici)');
-    });
-});
