990d6b360466ccb075a9243b0cebeec474c4107e
Jeddi Tratrick <legendes.du.monde.de.jeddi@gmail.com>
2025-12-12 18:31:57 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
+let currentLang = "fr";
+let translations = {};
+
+async function setLanguage(lang) {
+    currentLang = lang;
+
+    const response = await fetch(`translations/${lang}.json`);
+    translations = await response.json();
+
+    translatePage();
+}
+
+function translatePage() {
+    document.querySelectorAll("[data-i18n]").forEach(el => {
+        const key = el.getAttribute("data-i18n");
+        if (translations[key]) {
+            el.textContent = translations[key];
+        }
+    });
+}
+
+setLanguage("fr");
 
