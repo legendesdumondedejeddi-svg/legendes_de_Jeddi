914453c2cec140bbeb07630111acb29898b3f2df
Jeddi Tratrick <legendes.du.monde.de.jeddi@gmail.com>
2025-12-13 18:03:27 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,20 +1,7 @@
-let currentLang = localStorage.getItem("lang") || "fr";
-
-function changeLang(lang) {
-    currentLang = lang;
-    localStorage.setItem("lang", lang);
-    loadTranslations();
+function lire(bouton) {
+    const texte = bouton.previousElementSibling.innerText;
+    const voix = new SpeechSynthesisUtterance(texte);
+    voix.lang = "fr-FR";
+    speechSynthesis.cancel();
+    speechSynthesis.speak(voix);
 }
-
-function loadTranslations() {
-    fetch(`translations/${currentLang}.json`)
-        .then(r => r.json())
-        .then(data => {
-            document.querySelectorAll("[data-i18n]").forEach(el => {
-                let key = el.getAttribute("data-i18n");
-                if (data[key]) el.innerHTML = data[key];
-            });
-        });
-}
-
-loadTranslations();
