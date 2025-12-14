a149132be9c2c81515a5391aea35c8a90a2aa1b8
Jeddi Tratrick <legendes.du.monde.de.jeddi@gmail.com>
2025-12-14 18:18:58 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,14 +2,27 @@ function loadLanguage(lang) {
     fetch(`translations/${lang}.json`)
         .then(res => res.json())
         .then(data => {
-            document.querySelectorAll("[data-i18n]").forEach(el => {
-                const key = el.getAttribute("data-i18n");
-                if (data[key]) {
-                    el.textContent = data[key];
+           document.addEventListener("DOMContentLoaded", () => {
+    const buttons = document.querySelectorAll(".play-audio");
+
+    buttons.forEach(button => {
+        button.addEventListener("click", () => {
+            const audioSrc = button.getAttribute("data-audio");
+            const audio = button.nextElementSibling;
+
+            // Stop tous les autres audios
+            document.querySelectorAll(".audio-player").forEach(a => {
+                if (a !== audio) {
+                    a.pause();
+                    a.currentTime = 0;
                 }
             });
+
+            audio.src = audioSrc;
+            audio.play();
         });
-}
+    });
+});
 
 document.querySelectorAll(".submenu a").forEach(link => {
     link.addEventListener("click", e => {
