2eb70b50b51471c5e9ef20b3d4126d7d18606003
Jeddi Tratrick <legendes.du.monde.de.jeddi@gmail.com>
2025-12-15 14:08:03 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,36 @@
+// main.js
+
+// Changer la langue du menu et des textes
+function changeLanguage(lang) {
+    fetch("translations/" + lang + ".json")
+        .then(response => response.json())
+        .then(data => {
+            // Menu
+            document.querySelector('a[href="index.html"]').textContent = data.menu.accueil;
+            document.querySelector('a[href="legendes.html"]').textContent = data.menu.legendes;
+            document.querySelector('a[href="apropos.html"]').textContent = data.menu.apropos;
+            document.querySelector('a[href="don.html"]').textContent = data.menu.don;
+            document.querySelector('.lang-switch span').textContent = data.menu.langue + " ▾";
+
+            // Page À propos
+            const aproposTitle = document.querySelector('#apropos-title');
+            const aproposText = document.querySelector('#apropos-text');
+            if (aproposTitle && aproposText) {
+                aproposTitle.textContent = data.apropos.title;
+                aproposText.textContent = data.apropos.texte;
+            }
+
+            // Page DON
+            const donTitle = document.querySelector('#don-title');
+            const donText = document.querySelector('#don-text');
+            if (donTitle && donText) {
+                donTitle.textContent = data.don.title;
+                donText.textContent = data.don.texte;
+            }
+        });
+}
+
+// Changer l’audio dans legendes.html
 function changeAudio(lang) {
     const audio = document.getElementById("audio-player");
     if (!audio) {
@@ -7,16 +40,4 @@ function changeAudio(lang) {
     audio.src = "audio/aubepin_" + lang + ".mp3";
     audio.load();
     audio.play();
-}
-
-
-function changeLang(lang) {
-    fetch("translations/" + lang + ".json")
-        .then(res => res.json())
-        .then(data => {
-            document.querySelectorAll("[data-i18n]").forEach(el => {
-                const key = el.getAttribute("data-i18n");
-                if (data[key]) el.innerText = data[key];
-            });
-        });
 }
