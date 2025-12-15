c1bb5be7c6248749c30f733ad57425cd03ae2771
Jeddi Tratrick <legendes.du.monde.de.jeddi@gmail.com>
2025-12-15 07:00:16 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,17 @@
 function changeAudio(lang) {
     const audio = document.getElementById("audio-player");
-
-    if (!audio) {
-        alert("Lecteur audio introuvable.");
-        return;
-    }
-
     audio.src = "audio/aubepin_" + lang + ".mp3";
     audio.load();
     audio.play();
+}
+
+function changeLang(lang) {
+    fetch("translations/" + lang + ".json")
+        .then(res => res.json())
+        .then(data => {
+            document.querySelectorAll("[data-i18n]").forEach(el => {
+                const key = el.getAttribute("data-i18n");
+                if (data[key]) el.innerText = data[key];
+            });
+        });
 }
